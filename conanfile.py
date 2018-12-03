684c2aefeaf01a9168c532a6b3d51c2c6f269c2b
Ryan Lavin <lavinrp@gmail.com>
2018-12-02 22:48:59 -0500

    further work on building tgui

--- conanfile.py
+++ conanfile.py
@@ -15,28 +15,38 @@ class PackagerTGUI(ConanFile):
     requires = "sfml/2.5.0@bincrafters/stable"
     so_version = '0.8'
 
+    _source_subfolder = 'source_subfolder'
+    _build_subfolder = 'build_subfolder'
+
     def source(self):
         self.run("git clone https://github.com/texus/TGUI.git")
-        self.run("cd TGUI && git checkout 0.8")
+        self.run("cd TGUI && git checkout 0.8 && cd ..")
         # its in source/tgui/CMakeLists.txt
-        tools.replace_in_file()
+        print(os.getcwd())
+        os.listdir(os.getcwd())
+        tools.replace_in_file("./TGUI/src/TGUI/CMakeLists.txt",
+         "if(DEFINED SFML_LIBRARIES)\n    # SFML found via FindSFML.cmake\n    target_include_directories(tgui PRIVATE ${SFML_INCLUDE_DIR})\n    target_link_libraries(tgui PRIVATE ${SFML_LIBRARIES} ${SFML_DEPENDENCIES})\nelse()\n    # SFML found via SFMLConfig.cmake\n    target_link_libraries(tgui PRIVATE sfml-graphics)\nendif()",
+         "include(${CMAKE_BINARY_DIR}/conanbuildinfo.cmake)\nconan_basic_setup()\ntarget_link_libraries(tgui ${CONAN_LIBS})")
 
     def build(self):
-        cmake = CMake(self.settings)
-        self.run('mkdir _build')
-        self.run('cd _build && cmake ../%s -DBUILD_SHARED_LIBS=%s -DCMAKE_INSTALL_PREFIX=../install %s' %
-            ("TGUI", 'ON' if self.options.shared else 'OFF', cmake.command_line)
-        )
-        if self.settings.os == 'Windows':
-            self.run('cd _build && cmake --build . %s --target install --config %s' % (cmake.build_config, self.settings.build_type))
+        if self.settings.compiler == 'Visual Studio':
+            with tools.vcvars(self.settings, force=True, filter_known_paths=False):
+                cmake = self.configure_cmake()
         else:
-            self.run('cd _build && cmake --build . %s -- -j2 install' % cmake.build_config)
+            cmake = self.configure_cmake()
+        cmake.build()
 
     def configure_cmake(self):
         cmake = CMake(self)
         cmake.definitions["TGUI_SHARED_LIBS"] = self.options.shared
         if self.options.cpp17:
             cmake.definitions["TGUI_USE_CPP17"] = True
+
+        #os.rename(self._source_subfolder + '/extlibs', self._source_subfolder + '/ext')
+        #cmake.configure(build_folder=self._build_subfolder)
+        #os.rename(self._source_subfolder + '/ext', self._source_subfolder + '/extlibs')
+        cmake.configure()
+        return cmake
 
     def add_libraries_from_pc(self, library, static=None):
         if static is None:
@@ -50,6 +60,7 @@ class PackagerTGUI(ConanFile):
         self.cpp_info.exelinkflags.extend(pkg_config.libs_only_other)
 
     def package(self):
+        # self.copy(pattern='License.md', dst='licenses', src=self._source_subfolder)
         self.copy('*.*', 'include', 'install/include', keep_path=True)
         self.copy(pattern='*.a', dst='lib', src='install/lib', keep_path=False)
         self.copy(pattern='*.so.' + self.so_version, dst='lib', src='install/lib', keep_path=False)
--- test_package/example.cpp
+++ test_package/example.cpp
@@ -1,6 +1,23 @@
-#include <iostream>
-#include "hello.h"
+#include <TGUI/TGUI.hpp>
 
-int main() {
-    hello();
+int main()
+{
+    sf::RenderWindow window{{800, 600}, "Window"};
+    tgui::Gui gui{window}; // Create the gui and attach it to the window
+
+    while (window.isOpen())
+    {
+        sf::Event event;
+        while (window.pollEvent(event))
+        {
+            if (event.type == sf::Event::Closed)
+                window.close();
+
+            gui.handleEvent(event); // Pass the event to the widgets
+        }
+
+        window.clear();
+        gui.draw(); // Draw all widgets
+        window.display();
+    }
 }
